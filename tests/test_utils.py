02064db0569a00c46c72507f8cb1cb74a7913c6d
Anandashankar Anil <anandasanil@gmail.com>
2025-04-08 12:40:10 +0200

    Fix tests, black again

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,6 +10,9 @@ log.root.addHandler(error_handler)
 
 def test_error_reporting_without_error():
     """Test that no error is raised when the log has no error messages"""
+    for handler in log.root.handlers:
+        if isinstance(handler, utils.ErrorCollectorHandler):
+            handler.error_messages = []
     log.info("Test info")
     log.warning("Warn message")
     try:
@@ -20,6 +23,9 @@ def test_error_reporting_without_error():
 
 def test_error_reporting_with_error():
     """Test error thrown when the log has error messages"""
+    for handler in log.root.handlers:
+        if isinstance(handler, utils.ErrorCollectorHandler):
+            handler.error_messages = []
     error_message = "Test error message"
     log.error(error_message)
     with pytest.raises(RuntimeError, match=f"Errors logged in DailyRead during execution\n{error_message}"):
