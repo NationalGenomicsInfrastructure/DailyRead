3423e82090e56a2bbd37b0cfef8f47fa2e918fa7
Anandashankar Anil <anandasanil@gmail.com>
2025-11-03 17:27:53 +0100

    Black

--- daily_read/statusdb.py
+++ daily_read/statusdb.py
@@ -10,9 +10,7 @@ class StatusDBSession(object):
     def __init__(self, config):
         display_url_string = f"https://{config.STHLM_STATUSDB_USERNAME}:*********@{config.STHLM_STATUSDB_URL}"
         cloudant = cloudant_v1.CloudantV1(
-            authenticator=CouchDbSessionAuthenticator(
-                config.STHLM_STATUSDB_USERNAME, config.STHLM_STATUSDB_PASSWORD
-            )
+            authenticator=CouchDbSessionAuthenticator(config.STHLM_STATUSDB_USERNAME, config.STHLM_STATUSDB_PASSWORD)
         )
         cloudant.set_service_url(config.STHLM_STATUSDB_URL)
         self.connection = cloudant
@@ -21,13 +19,8 @@ class StatusDBSession(object):
         except Exception as e:
             raise ConnectionError(f"Couchdb connection failed for url {display_url_string} with error {e}")
 
-
     def rows(self, close_date=None):
         view = self.connection.post_view(
-            db="projects",
-            ddoc="project",
-            view="dailyread_dates",
-            descending=True,
-            end_key=[close_date, "ZZZZ"]
+            db="projects", ddoc="project", view="dailyread_dates", descending=True, end_key=[close_date, "ZZZZ"]
         ).get_result()
         return view["rows"]
